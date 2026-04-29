95256663df5ec1497e2c9e637ad071efcb00cbb2
danil <danilaorlovic28@gmail.com>
2026-04-30 02:34:27 +0300

    fix: remove patch

--- Course/Program.cs
+++ Course/Program.cs
@@ -22,17 +22,16 @@ builder.Services.AddScoped<IService,Service>();
 
 var app = builder.Build();
 
-if(false)
-    using (var scope = app.Services.CreateScope())
-    {
-        Console.WriteLine("Sleep");
-        Thread.Sleep(5000);
-        Console.WriteLine("Wake up");
+using (var scope = app.Services.CreateScope())
+{
+    Console.WriteLine("Sleep");
+    Thread.Sleep(5000);
+    Console.WriteLine("Wake up");
 
-        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-        db.Database.EnsureDeleted();
-        db.Database.EnsureCreated();
-    }
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureDeleted();
+    db.Database.EnsureCreated();
+}
 
 app.UseHttpsRedirection();
 app.UseRouting();
