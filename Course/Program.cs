5c6899776e610a85047aa43a6e4f53e64c44f6b7
danil <danilaorlovic28@gmail.com>
2026-05-06 14:25:09 +0300

    fix: create new db

--- Course/Program.cs
+++ Course/Program.cs
@@ -48,13 +48,13 @@ builder.Services.AddScoped<IAccountService, AccountService>();
 var app = builder.Build();
 
 app.UseStaticFiles();
-//using (var scope = app.Services.CreateScope())
-//{
-//    Thread.Sleep(5000);
-//    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-//    db.Database.EnsureDeleted();
-//    db.Database.EnsureCreated();
-//}
+using (var scope = app.Services.CreateScope())
+{
+    Thread.Sleep(5000);
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureDeleted();
+    db.Database.EnsureCreated();
+}
 
 using (var scope = app.Services.CreateScope())
 {
