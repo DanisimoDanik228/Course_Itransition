832ae499fe870ec3a8bb6cc47f2542e2016c9df2
danil <danilaorlovic28@gmail.com>
2026-05-21 12:48:47 +0300

    feat: save theme

--- Course/Program.cs
+++ Course/Program.cs
@@ -46,7 +46,7 @@ builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
 .AddEntityFrameworkStores<AppDbContext>()
 .AddDefaultTokenProviders();
 
-var settings = new ElasticsearchClientSettings(new Uri("http://elasticsearch:9200"))
+var settings = new ElasticsearchClientSettings(new Uri("http://localhost:9200"))
     .DefaultIndex("editor_index");
 
 builder.Services.Configure<InventorySettings>(
@@ -72,13 +72,13 @@ builder.Services.AddScoped<ICustomIdService, CustomIdService>();
 var app = builder.Build();
 
 app.UseStaticFiles();
-using (var scope = app.Services.CreateScope())
-{
-    Thread.Sleep(30 * 1000);
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureDeleted();
-    db.Database.EnsureCreated();
-}
+//using (var scope = app.Services.CreateScope())
+//{
+//    //Thread.Sleep(30 * 1000);
+//    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+//    db.Database.EnsureDeleted();
+//    db.Database.EnsureCreated();
+//}
 
 using (var scope = app.Services.CreateScope())
 {
